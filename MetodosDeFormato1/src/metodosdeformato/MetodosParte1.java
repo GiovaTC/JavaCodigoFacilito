da6f48c4187ac8e60fe6eff2dd5eec29111a12ac
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-20 16:02:25 -0500

    Add files via upload

---
+++ MetodosDeFormato1/src/metodosdeformato/MetodosParte1.java
@@ -0,0 +1,21 @@
+package metodosdeformato;
+
+public class MetodosParte1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// String Clase
+		
+		String mensaje = "     Hola, soy un String    ";
+		
+		System.out.println(mensaje.toUpperCase().trim()); // mayusculas
+		System.out.println(mensaje.toLowerCase().trim()); // minusculas
+		
+		System.out.println(mensaje.trim()); // reducir espacios
+		
+		System.out.println(mensaje);
+
+		
+	}
+
+}
