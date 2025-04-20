735d06049bcb2877618c1ac8cc49950f17bcada8
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-20 15:43:44 -0500

    Add files via upload

--- MetodosCadenas/src/cadenas/MetodosCadenas.java
+++ MetodosCadenas/src/cadenas/MetodosCadenas.java
@@ -10,8 +10,21 @@ public class MetodosCadenas {
 		
 		int cantidad = mensaje.length();
 		
-		System.out.println(mensaje);
-
+		boolean contiene = mensaje.contains("u");
+		
+		boolean comienzaCon = mensaje.startsWith("H");
+		
+		boolean terminaCon = mensaje.endsWith("g");
+		
+		mensaje.startsWith("Hola");
+		
+		System.out.println(cantidad);
+		System.out.println(contiene);
+		System.out.println(comienzaCon);
+		System.out.println(terminaCon);
+		
+		String nuevoString = mensaje.concat(", estamos en el curso de Java");
+        System.out.println(nuevoString);
 	}
 
 }
