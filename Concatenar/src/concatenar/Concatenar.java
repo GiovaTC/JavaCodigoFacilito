205e23bae1312e52752012eee54dcbd6581686d8
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-20 16:43:28 -0500

    Add files via upload

---
+++ Concatenar/src/concatenar/Concatenar.java
@@ -0,0 +1,16 @@
+package concatenar;
+
+public class Concatenar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Concatenar String
+		String nombre = "Codi";
+		String curso = "Curso de Java 9";
+		
+		String mensaje = "Hola, bienvenido " + nombre + " al " + curso + ".";
+		System.out.println(mensaje);
+
+	}
+
+}
