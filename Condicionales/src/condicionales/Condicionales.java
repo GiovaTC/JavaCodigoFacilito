20921cc78e8d4a27bf3ed0d6dfd73c7b02296f2c
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-21 11:01:22 -0500

    Add files via upload

---
+++ Condicionales/src/condicionales/Condicionales.java
@@ -0,0 +1,21 @@
+package condicionales;
+
+public class Condicionales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String colorLuz = "Verde";
+		
+		if (colorLuz.equals("Verde"))
+		{
+			System.out.println("Puede continuar");
+		
+		} else {
+			
+			System.out.println("Alto total");
+		}
+
+	}
+
+}
