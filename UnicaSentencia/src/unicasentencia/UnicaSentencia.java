b8fd8f62d7b7510605c84488fa78a06535e3b0b3
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-22 18:43:38 -0500

    Add files via upload

---
+++ UnicaSentencia/src/unicasentencia/UnicaSentencia.java
@@ -0,0 +1,26 @@
+package unicasentencia;
+
+public class UnicaSentencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean valor = true;
+		
+		if (valor) 
+		{
+			System.out.println("Es verdadero!");
+			System.out.println("Es verdadero!");
+		} else 
+		{
+			System.out.println("Es falso!");
+		}
+		
+		for ( int x = 0; x < 10; x++)
+		{
+			System.out.println("Mensaje!");
+		}
+
+	}
+
+}
