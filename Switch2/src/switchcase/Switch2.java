04531c2ec3972dc59528cee9278874d10334492e
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-21 19:02:06 -0500

    Add files via upload

---
+++ Switch2/src/switchcase/Switch2.java
@@ -0,0 +1,28 @@
+package switchcase;
+
+public class Switch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Switch
+		char calificacion = 'D';
+		
+		switch(calificacion)
+		{
+		case 'A':
+			System.out.println("Excelente!");
+			break;
+		case 'B':
+			System.out.println("Bien hecho");
+			break;
+		case 'C':
+			System.out.println("Bien hecho");
+			break;
+		case 'D':
+			System.out.println("Aprobatorio");
+			break;
+		}
+
+	}
+
+}
