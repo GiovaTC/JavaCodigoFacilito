7326590957cf0d5118c39719248f08d19da0b471
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-22 12:57:35 -0500

    Add files via upload

---
+++ CicloFor/src/ciclos/CicloFor.java
@@ -0,0 +1,15 @@
+package ciclos;
+
+public class CicloFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Ciclo For
+		
+		// for ( Inicialización; Condición; Iteración ) { }
+		
+		for(int numero=9, x = 1; x < 11; System.out.println(numero * x), x++) {}
+
+	}
+
+}
