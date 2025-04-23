7bd2505e05ea1d09345cb2bd62076f2aa0963961
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-23 13:13:10 -0500

    Add files via upload

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,21 @@
+package arreglos;
+
+public class Arreglos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombres[];
+		nombres = new String[3];
+		// 3 = 0, 1, 2
+		
+		nombres[0] = "Codi 1";
+		nombres[1] = "Codi 2";
+		nombres[2] = "Codi 3";
+		
+		System.out.println( nombres[0]);
+		System.out.println( nombres[1]);
+		System.out.println( nombres[2]);
+	}
+
+}
