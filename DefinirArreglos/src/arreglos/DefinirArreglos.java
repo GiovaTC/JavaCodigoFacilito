bc6ee1e8f76fe47c0c73ec4845d4afcccc4c5be2
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-23 13:19:09 -0500

    Add files via upload

---
+++ DefinirArreglos/src/arreglos/DefinirArreglos.java
@@ -0,0 +1,19 @@
+package arreglos;
+
+public class DefinirArreglos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//nombres = new String[3];
+		
+		//String nombres[] = new String[3];
+		
+		String nombres[] = { "Codi 1", "Codi 2", "Codi 3" };
+		
+		System.out.println( nombres[0] );
+		System.out.println( nombres[1] );
+		System.out.println( nombres[2] );
+//		System.out.println( nombres[3] );
+	}
+
+}
