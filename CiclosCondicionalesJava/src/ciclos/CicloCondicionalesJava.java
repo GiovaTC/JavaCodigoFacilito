df5ecfbbfe1a9dd801788eb6bcd590a454b015d7
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-22 19:18:16 -0500

    Add files via upload

--- CiclosCondicionalesJava/src/ciclos/CicloCondicionalesJava.java
+++ CiclosCondicionalesJava/src/ciclos/CicloCondicionalesJava.java
@@ -4,11 +4,12 @@ public class CicloCondicionalesJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	    // Ejercicio calificacion de un estudiante
 		
 		String mensaje  = "aprobado";
 		String mensaje2 = "reprobado";
 		
-		int calificacion = 50;
+		int calificacion = 70;
 		
 		if (calificacion >= 70)
 		{
@@ -21,3 +22,4 @@ public class CicloCondicionalesJava {
 	}
 
 }
+
