887d8582f3430ed9e5f419862f4fc0b5db9f3626
Giovanny Alejandro <90740161+GiovaTC@users.noreply.github.com>
2025-04-25 11:59:26 -0500

    Add files via upload

--- ClasesyObjetos/src/clasesobj/ClasesyObjetos.java
+++ ClasesyObjetos/src/clasesobj/ClasesyObjetos.java
@@ -15,6 +15,10 @@ public class ClasesyObjetos {
 		System.out.println("El nombre es : " + lassie.nombre);
 		System.out.println("La raza es : " + lassie.raza);
 		System.out.println("La edad es : " + lassie.edad);
+		
+		lassie.comer();
+		lassie.ladrar();
+		lassie.dormir();
 	}
 
 }
--- ClasesyObjetos/src/clasesobj/Perro.java
+++ ClasesyObjetos/src/clasesobj/Perro.java
@@ -5,5 +5,20 @@ public class Perro {
 	String nombre;
 	String raza;
 	int edad;
+	
+	void comer()
+	{
+		System.out.println("El perro se encuentra comiendo!");
+	}
+	
+	void dormir()
+	{
+		System.out.println("El perro se encuentra dormido!");
+	}
+	
+	void ladrar()
+	{
+		System.out.println("El perro se encuentra ladrando!");
+	}
 
 }
